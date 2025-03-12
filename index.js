133175f458a6b298e01ceffe2f482e03440d98fc
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-12 23:53:05 +0600

    factorial recursion problem

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-// char count problem
-let str = "hello everyone";
-let obj = {};
-
-function charCount(str) {
-  for (let x of str) {
-    if (x !== " ") {
-      if (!obj.hasOwnProperty(x)) {
-        obj[x] = 1;
-      } else {
-        obj[x] = obj[x] + 1;
-      }
-    }
+function factorial(n) {
+  if (n <= 1) {
+    return 1;
   }
 
-  return obj;
+  return n * factorial(n - 1);
 }
 
-console.log(charCount(str));
+console.log(factorial(5));
