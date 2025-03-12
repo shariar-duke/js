d58314d69819269218fd41601ae13912301af6cb
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-12 21:06:48 +0600

    my solution

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Shariar Mahamud Duke");
+const mixedArray = [12, -7, 45, "hello", -23, 89, "world", -3];
+
+let max = -Infinity;
+let min = Infinity;
+
+for (let x of mixedArray) {
+  if (typeof x === "number") {
+    if (x > max) {
+      max = x;
+    }
+  }
+}
+console.log("Max value is", Infinity);
