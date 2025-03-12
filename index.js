23c01c36dc2d3d36efbc9b0d5f61d841bbd47ac0
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-12 22:46:44 +0600

    second maximum number find problem

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const mixedArray = [12, -7, 45, "hello", -23, 89, "world", -3];
 
 let max = -Infinity;
+let secondmax = -Infinity;
 let min = Infinity;
 
 for (let x of mixedArray) {
   if (typeof x === "number") {
     if (x > max) {
+      secondmax = max;
       max = x;
     }
   }
 }
-console.log("Max value is", Infinity);
+console.log("Max value is", max);
+console.log("Second Max is", secondmax);
