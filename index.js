1049a93fb8abcf4502f7ed0c95b4c1ffcde3d7a0
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-13 01:49:18 +0600

    Binary Search

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function linearSearch(arr, value) {
-  for (let x = 0; x < arr.length; x++) {
-    if (arr[x] === value) {
-      return x;
+let arr = [3, 5, 6, 9, 44, 56, 99, 102, 150, 170];
+
+function binarySearch(arr, num) {
+  if (arr.length < 1) {
+    return -1;
+  }
+
+  let start = 0;
+  let end = arr.length - 1;
+  let mid;
+
+  while (start <= end) {
+    mid = Math.floor((start + end) / 2);
+    if (arr[mid] === num) {
+      return mid;
+    } else if (num > mid) {
+      start = mid + 1;
+    } else if (num < mid) {
+      end = mid - 1;
     }
   }
 
-  return -1;
+  return -1; // jode number khuje na pay then eta
 }
 
-console.log(linearSearch([2, 3, 5, 6, 9], 9));
+console.log(binarySearch(arr, 170)); // Output: 9
