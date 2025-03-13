5ff3bbe891f857379772885ff0b5014dcd62b440
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-13 15:30:25 +0600

    basic queue

--- index.js
+++ index.js
@@ -1,60 +1,31 @@
-class Stack {
+class Queue {
   constructor() {
     this.arr = [];
   }
 
-  // Add an element to the stack
-  push(num) {
+  enque(num) {
     this.arr.push(num);
   }
 
-  // Remove and return the top element from the stack
-  pop() {
-    if (this.isEmpty()) {
-      return "Stack is empty";
-    }
-    return this.arr.pop();
+  deQueue() {
+    this.arr.shift();
   }
 
-  // Return the top element without removing it
-  peek() {
-    if (this.isEmpty()) {
-      return "Stack is empty";
-    }
-    return this.arr[this.arr.length - 1];
-  }
-
-  // Check if the stack is empty
-  isEmpty() {
-    return this.arr.length === 0;
-  }
-
-  // Return the size of the stack
-  size() {
-    return this.arr.length;
-  }
-
-  // Clear the stack
-  clear() {
-    this.arr = [];
-  }
-
-  // Show the current stack
-  showStack() {
+  showQueue() {
     return this.arr;
   }
 }
 
-let stack = new Stack();
-stack.push(5);
-stack.push(12);
-stack.push(20);
+const queue = new Queue();
 
-console.log("Stack:", stack.showStack()); // [5, 12, 20]
-console.log("Peek:", stack.peek()); // 20
-console.log("Pop:", stack.pop()); // 20
-console.log("Stack after pop:", stack.showStack()); // [5, 12]
-console.log("Size of stack:", stack.size()); // 2
-console.log("Is stack empty?", stack.isEmpty()); // false
-stack.clear();
-console.log("Stack after clear:", stack.showStack()); // []
+// Enquee
+queue.enque(2);
+queue.enque(3);
+queue.enque(4);
+queue.enque(5);
+queue.enque(10);
+
+//deQuee
+queue.deQueue();
+
+console.log(queue.showQueue());
