68fd9e5787085616ed516e8d054a0226bab78605
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-14 15:20:26 +0600

    clean code of adding left and right child both

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// crating Node element
+// Creating Node element
 class Node {
   constructor(value) {
     this.value = value;
@@ -13,13 +13,43 @@ class BinarySearchTree {
   }
 
   addChild(value) {
-    if (this.root === null) {
+    if (!this.root) {
       this.root = new Node(value);
-      return;
+      return `Node ${value} added as root`;
+    }
+
+    let currentNode = this.root;
+
+    while (true) {
+      if (currentNode.value === value) {
+        console.log("Duplicate Node not allowed");
+        return "Duplicate Node is not allowed";
+      }
+
+      if (value < currentNode.value) {
+        // Move to the left child
+        if (!currentNode.leftChild) {
+          currentNode.leftChild = new Node(value);
+          return `Node ${value} added to the left of ${currentNode.value}`;
+        }
+        currentNode = currentNode.leftChild;
+      } else {
+        // Move to the right child
+        if (!currentNode.rightChild) {
+          currentNode.rightChild = new Node(value);
+          return `Node ${value} added to the right of ${currentNode.value}`;
+        }
+        currentNode = currentNode.rightChild;
+      }
     }
   }
 }
 
+// Example usage
 const bst = new BinarySearchTree();
-bst.addChild(10);
+console.log(bst.addChild(10)); // Node 10 added as root
+console.log(bst.addChild(5)); // Node 5 added to the left of 10
+console.log(bst.addChild(15)); // Node 15 added to the right of 10
+console.log(bst.addChild(5)); // Duplicate Node not allowed
+
 console.log(bst.root);
